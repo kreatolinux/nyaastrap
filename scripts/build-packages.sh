8dac97325a5730e682ae57f473395a3da4240c4c
Kreato <kerimkocabas@tutanota.com>
2022-11-02 05:04:37 +0300

    add some comamnds to build-packages

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -3,4 +3,7 @@ echo "nyaastrap - compile packages mode"
 git config --global --add safe.directory /etc/nyaa
 cd /etc/nyaa
 . /etc/profile
+rm -rf /etc/nyaa.installed
+ln -s $(which gcc) /bin/cc
+ln -s $(which gcc) /bin/c99
 sh build-everything.sh
