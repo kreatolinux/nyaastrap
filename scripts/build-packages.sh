044e628873a622b3fa6873b7f71bfeb6a4ee0b6a
Kreato <kerimkocabas@tutanota.com>
2022-11-03 01:33:05 +0300

    these commands arent needed anymore

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -4,6 +4,4 @@ git config --global --add safe.directory /etc/nyaa
 cd /etc/nyaa
 . /etc/profile
 rm -rf /etc/nyaa.installed
-ln -s $(which gcc) /bin/cc
-ln -s $(which gcc) /bin/c99
 sh build-everything.sh
