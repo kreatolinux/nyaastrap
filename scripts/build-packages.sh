a7bbdc0c3c6ec552bbc801f012b8e81cde780409
Kreato <kerimkocabas@tutanota.com>
2022-11-06 07:08:05 +0300

    new scripts

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 echo "nyaastrap - compile packages mode"
 git config --global --add safe.directory /etc/nyaa
-cd /etc/nyaa
 . /etc/profile
 rm -rf /etc/nyaa.installed
-sh build-everything.sh
+cd /nyaa/scripts || exit 1
+sh test_bin.sh -o test.txt
+cd /nyaa/nyaa-repo || exit 1
+for i in *
+do
+    if [ -d "$i" ]; then
+        . "$i/run"
+        while read line
+        do
+            [ "nyaa-tarball-$i-$VERSION.tar.gz" = "$line" ] && nyaa b $i -y
+        done < /nyaa/scripts/test.txt
+    fi
+done
+rm -f /nyaa/scripts/test.txt
